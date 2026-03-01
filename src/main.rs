bb92cc3048356b4ad5342ed68cb6f21d9ba00127
Matt Prodani <mattp@hey.com>
2026-03-01 14:11:15 -0500

    fix: use multiplexed input protocol from uefi::system::with_stdin
    Prior to this we were using open_protocol_exclusive with Input GUID,
    this works with OVMF in QEMU, but in essence it was pulling a random input
    device. I saw no input when trying on my actual system.
    uefi::system::with_stdin makes sure the input device passed is the
    system multiplexed input.

--- src/main.rs
+++ src/main.rs
@@ -15,10 +15,7 @@ use plex_boot::core::bootables::BootTarget;
 use plex_boot::core::display::GopDisplay;
 use plex_boot::path::DiskManager;
 use plex_boot::ui;
-use uefi::{
-    prelude::*,
-    proto::console::{gop::GraphicsOutput, text::Input},
-};
+use uefi::{prelude::*, proto::console::gop::GraphicsOutput};
 
 #[entry]
 fn main() -> Status {
@@ -46,21 +43,23 @@ fn main() -> Status {
     let gop_handle = boot::get_handle_for_protocol::<GraphicsOutput>().unwrap();
     let mut gop = boot::open_protocol_exclusive::<GraphicsOutput>(gop_handle).unwrap();
 
-    let input_handle = boot::get_handle_for_protocol::<Input>().unwrap();
-    let mut input = boot::open_protocol_exclusive::<Input>(input_handle).unwrap();
-
-    let mut display = GopDisplay::new(&mut gop);
-    let mut app_ctx = AppCtx {
-        display: &mut display,
-        input: &mut input,
-        disk_manager: &disk_manager,
-        handle,
-    };
-    let mut menu = ui::boot_menu::BootMenu::<BootTarget>::new(boot_targets.as_mut_slice());
-    if let AppResult::Error(ref err) = menu.run(&mut app_ctx) {
-        let mut overlay = ui::overlay::ErrorOverlay::new(err);
-        let _ = overlay.run(&mut app_ctx);
-    }
+    // Using with_stdin ensures we choose the multiplexed EFI input device,
+    // otherwise we might pick up a specific device that is not the main keyboard
+    // and miss inputs.
+    uefi::system::with_stdin(|input| {
+        let mut display = GopDisplay::new(&mut gop);
+        let mut app_ctx = AppCtx {
+            display: &mut display,
+            input,
+            disk_manager: &disk_manager,
+            handle,
+        };
+        let mut menu = ui::boot_menu::BootMenu::<BootTarget>::new(boot_targets.as_mut_slice());
+        if let AppResult::Error(ref err) = menu.run(&mut app_ctx) {
+            let mut overlay = ui::overlay::ErrorOverlay::new(err);
+            let _ = overlay.run(&mut app_ctx);
+        }
+    });
 
     Status::SUCCESS
 }
